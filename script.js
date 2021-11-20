e7b0eee7063e74d6b88e5e80203a30db573090fd
Glitch (efficacious-eight-canary) <none>
2021-11-20 23:34:39 +0000

    🎿🌒 Checkpoint ./script.js:38828744/3764 ./style.css:38828744/2576

--- script.js
+++ script.js
@@ -35,3 +35,63 @@ function nameUnhovered() {
 function nameClicked() {
   document.getElementById("target").scrollIntoView(true, {behavior: "smooth"})
 }
+
+
+
+
+//Scroll
+const numSteps = 20.0;
+
+let boxElement;
+let nameElement;
+let prevRatio = 0.0;
+let increasingColor = "rgba(40, 40, 190, ratio)";
+let decreasingColor = "rgba(190, 40, 40, ratio)";
+
+// Set things up
+window.addEventListener("load", (event) => {
+  boxElement = document.querySelector("#box");
+  nameElement = document.querySelector("#name");
+
+  createObserver();
+}, false);
+
+function createObserver() {
+  let observer;
+
+  let options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: buildThresholdList()
+  };
+
+  observer = new IntersectionObserver(handleIntersect, options);
+  observer.observe(boxElement);
+  observer.observe(nameElement);
+}
+
+function buildThresholdList() {
+  let thresholds = [];
+  let numSteps = 20;
+
+  for (let i=1.0; i<=numSteps; i++) {
+    let ratio = i/numSteps;
+    thresholds.push(ratio);
+  }
+
+  thresholds.push(0);
+  return thresholds;
+}
+
+function handleIntersect(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio > prevRatio) {
+      entry.target.style.color = increasingColor.replace("ratio", entry.intersectionRatio);
+      
+    } else {
+      entry.target.style.color = decreasingColor.replace("ratio", entry.intersectionRatio);
+    }
+
+    prevRatio = entry.intersectionRatio;
+  });
+}
