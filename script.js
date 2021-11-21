b4282203b041b8dd90616efb6da68f3c3c1b3d87
Glitch (efficacious-eight-canary) <none>
2021-11-21 22:16:53 +0000

    💘🎠 Checkpoint ./script.js:38828744/54

--- script.js
+++ script.js
@@ -70,8 +70,8 @@ function handleIntersect(entries, observer) {
      // Scrolling down/up
     if (currentY < previousY) {
       if (currentRatio > previousRatio && isIntersecting) { // Scrolling down enter
-            //entry.target.style.transform = `translate(0, ${(-1 * entry.intersectionRatio) * 150}px)`
-        entry.target.style.transform = `translate(0, 0)`
+        entry.target.style.transform = `translate(0, ${(-1 * entry.intersectionRatio) * 150}px)`
+        
       } else { // Scrolling down leave
             entry.target.style.transform = `translate(0, ${(entry.intersectionRatio) * 150}px)`
       }
