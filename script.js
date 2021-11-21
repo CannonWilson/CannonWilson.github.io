2a9d5e465f5421c048e2c44825a38011a59ffb64
Glitch (efficacious-eight-canary) <none>
2021-11-21 21:08:18 +0000

    ⏲📰 Checkpoint ./style.css:38828744/9 ./index.html:38828744/299 ./script.js:38828744/11151

--- script.js
+++ script.js
@@ -16,117 +16,118 @@ function loadStars(star, starsToLoad) {
 
 
 //Scroll first block tried
-// const numSteps = 20.0;
+const numSteps = 20.0;
 
 // let boxElement;
-// let nameElement;
-// let prevRatio = 0.0;
+let nameElement;
+let previousRatio = 0.0;
+let previousY = 0;
 // let increasingColor = "rgba(40, 40, 190, ratio)";
 // let decreasingColor = "rgba(190, 40, 40, ratio)";
 
-// // Set things up
-// window.addEventListener("load", (event) => {
-//   boxElement = document.querySelector("#box");
-//   nameElement = document.querySelector("#name");
+// Set things up
+window.addEventListener("load", (event) => {
+  // boxElement = document.querySelector("#box");
+  nameElement = document.querySelector("#name");
 
-//   createObserver();
-// }, false);
+  createObserver();
+}, false);
 
-// function createObserver() {
-//   let observer;
+function createObserver() {
+  let observer;
 
-//   let options = {
-//     root: null,
-//     rootMargin: "0px",
-//     threshold: buildThresholdList()
-//   };
+  let options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: buildThresholdList()
+  };
 
-//   observer = new IntersectionObserver(handleIntersect, options);
-//   observer.observe(boxElement);
-//   observer.observe(nameElement);
-// }
+  observer = new IntersectionObserver(handleIntersect, options);
+  // observer.observe(boxElement);
+  observer.observe(nameElement);
+}
 
-// function buildThresholdList() {
-//   let thresholds = [];
-//   let numSteps = 20;
+function buildThresholdList() {
+  let thresholds = [];
+  let numSteps = 20;
 
-//   for (let i=1.0; i<=numSteps; i++) {
-//     let ratio = i/numSteps;
-//     thresholds.push(ratio);
-//   }
+  for (let i=1.0; i<=numSteps; i++) {
+    let ratio = i/numSteps;
+    thresholds.push(ratio);
+  }
 
-//   thresholds.push(0);
-//   return thresholds;
-// }
+  thresholds.push(0);
+  return thresholds;
+}
 
-// function handleIntersect(entries, observer) {
-//   entries.forEach((entry) => {
+function handleIntersect(entries, observer) {
+  entries.forEach((entry) => {
     
-//       entry.target.style.opacity = `${entry.intersectionRatio}`;      
-//       if (entry.target.scrollHeight - entry.target.scrollTop === entry.target.clientHeight - 1) {
-//               entry.target.style.transform = `translate(0, ${(-1*entry.intersectionRatio) * 150}px)`
-//       }
-
-      
-//     if (entry.intersectionRatio > prevRatio) {
-//       //entry.target.style.backgroundColor = increasingColor.replace("ratio", entry.intersectionRatio);
-//       //entry.target.style.fontSize = `${Math.floor(entry.intersectionRatio * 14)}vw`;
-//       //entry.target.style.opacity = `${entry.intersectionRatio}`;
-//       //entry.target.style.top = `${(entry.intersectionRatio - 0.5) * 200}px`
-//       //entry.target.style.transform = `translate(0, ${(entry.intersectionRatio) * 150}px)`
-//     } else {
-//       //entry.target.style.backgroundColor = decreasingColor.replace("ratio", entry.intersectionRatio);
-//       //entry.target.style.fontSize = `${Math.floor(entry.intersectionRatio * 14)}vw`;
-//       //entry.target.style.opacity = `${entry.intersectionRatio}`;
-//       //entry.target.style.top = `${(entry.intersectionRatio - 0.5) * 200}px`
-//       //entry.target.style.transform = `translate(0, ${(entry.intersectionRatio) * 150}px)`
-//     }
-
-//     prevRatio = entry.intersectionRatio;
-//   });
-// }
-
-
-
-// SCroll second aproach tried
-const state = document.querySelector('.observer__state')
-const target = document.querySelector('.observer__target')
-
-const thresholdArray = steps => Array(steps + 1)
- .fill(0)
- .map((_, index) => index / steps || 0)
-
-let previousY = 0
-let previousRatio = 0
-
-const handleIntersect = entries => {
-  entries.forEach(entry => {
     const currentY = entry.boundingClientRect.y
     const currentRatio = entry.intersectionRatio
     const isIntersecting = entry.isIntersecting
-
-    // Scrolling down/up
+    
+     // Scrolling down/up
     if (currentY < previousY) {
       if (currentRatio > previousRatio && isIntersecting) { // Scrolling down enter
-        state.textContent ="Scrolling down enter"
+            entry.target.style.transform = `translate(0, ${(-1*entry.intersectionRatio) * 150}px)`
       } else { // Scrolling down leave
-        state.textContent ="Scrolling down leave"
+            entry.target.style.transform = `translate(0, ${(-1*entry.intersectionRatio) * 150}px)`
       }
     } else if (currentY > previousY && isIntersecting) {
       if (currentRatio < previousRatio) { // Scrolling up leave
-        state.textContent ="Scrolling up leave"
+            entry.target.style.transform = `translate(0, ${(-1*entry.intersectionRatio) * 150}px)`
       } else { // Scrolling up enter
-        state.textContent ="Scrolling up enter"
+            entry.target.style.transform = `translate(0, ${(-1*entry.intersectionRatio) * 150}px)`
       }
     }
 
     previousY = currentY
     previousRatio = currentRatio
-  })
+  });
 }
 
-const observer = new IntersectionObserver(handleIntersect, {
-  threshold: thresholdArray(20),
-})
 
-observer.observe(target)
+
+// SCroll second aproach tried
+// const state = document.querySelector('.observer__state')
+// const target = document.querySelector('.observer__target')
+
+// const thresholdArray = steps => Array(steps + 1)
+//  .fill(0)
+//  .map((_, index) => index / steps || 0)
+
+// let previousY = 0
+// let previousRatio = 0
+
+// const handleIntersect = entries => {
+//   entries.forEach(entry => {
+//     const currentY = entry.boundingClientRect.y
+//     const currentRatio = entry.intersectionRatio
+//     const isIntersecting = entry.isIntersecting
+
+//     // Scrolling down/up
+//     if (currentY < previousY) {
+//       if (currentRatio > previousRatio && isIntersecting) { // Scrolling down enter
+//         state.textContent ="Scrolling down enter"
+//       } else { // Scrolling down leave
+//         state.textContent ="Scrolling down leave"
+//       }
+//     } else if (currentY > previousY && isIntersecting) {
+//       if (currentRatio < previousRatio) { // Scrolling up leave
+//         state.textContent ="Scrolling up leave"
+//       } else { // Scrolling up enter
+//         state.textContent ="Scrolling up enter"
+//       }
+//     }
+
+//     previousY = currentY
+//     previousRatio = currentRatio
+//   })
+// }
+
+// const observer = new IntersectionObserver(handleIntersect, {
+//   threshold: thresholdArray(20),
+// })
+
+// observer.observe(target)
