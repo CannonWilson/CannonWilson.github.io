fc9228f96bad50c60aad3ef3cd0478d7b0fb4530
Glitch (efficacious-eight-canary) <none>
2021-11-22 16:31:25 +0000

    Pushing again

--- script.js
+++ script.js
@@ -101,7 +101,7 @@ const [red, green, blue] = [225, 111, 225]
 let y=0
 
 function scrollHandler () {
-  const y = (1 + document.getElementById("vertical-scroll-wrapper").scrollTop)/1500 // was 150, or 2500 or 3000
+  const y = (document.getElementById("vertical-scroll-wrapper").scrollTop)/1500 // was 150, or 2500 or 3000
   const [r, g, b] = [red*y, green*y, blue/y].map(Math.round) // was division instead of multiplication
   document.body.style.background = `linear-gradient(to top, rgb(${r}, ${g}, ${b}) -140%, black) center center fixed`
 }
